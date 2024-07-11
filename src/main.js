c0852f3e1c6a0ca5a4df1e656c5d362efc18606c
xAndreySmx <andrey.sm.wwork@gmail.com>
2024-07-11 16:45:19 +0300

    Updates

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import VueSmoothScroll from "vue3-smooth-scroll";
-import App from "./App.vue";
-const app = createApp(App);
-app.use(VueSmoothScroll); //подключение vue-smooth-scroll
-app.mount("#app");
